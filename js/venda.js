1e2e6056f7dbae4b679357a0a843ba3ffd639cb0
danieljantunes1 <dani.j.antunes1@gmail.com>
2024-07-21 13:59:09 -0300

    s

--- js/venda.js
+++ js/venda.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Seleciona as imagens
-    const imgGrande = document.querySelector('#fotinha1 img');
-    const imgPequena = document.querySelector('#fotinha2 img');
-
-    // Adiciona o evento de clique à imagem pequena para trocar as imagens
-    imgPequena.addEventListener('click', function () {
-        const grandeSrc = imgGrande.src;
-        const pequenaSrc = imgPequena.src;
-
-        imgGrande.src = pequenaSrc;
-        imgPequena.src = grandeSrc;
-    });
-
-    // Código para o carrinho de compras
     const cartIcon = document.getElementById('cart-icon');
     const cartCount = document.getElementById('cart-count');
     const sizeBoxes = document.querySelectorAll('.size-box');
@@ -21,6 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function updateCartCount() {
         cartCount.textContent = cart.length;
+        if (cart.length > 0) {
+            cartCount.style.display = 'block'; // Exibe o contador se houver itens no carrinho
+        } else {
+            cartCount.style.display = 'none'; // Oculta o contador se estiver vazio
+        }
     }
 
     sizeBoxes.forEach(box => {
@@ -38,19 +29,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         const item = {
-            product: document.querySelector('h3').textContent,
-            color: document.querySelector('h4').textContent,
+            product: document.querySelector('h4').textContent,
+            color: document.querySelector('h5').textContent,
             size: selectedSize,
             price: parseFloat(document.querySelector('#preço1 p').textContent.replace('R$ ', '').replace(',', '.'))
         };
         cart.push(item);
         localStorage.setItem('cart', JSON.stringify(cart));
-        updateCartCount();
+        updateCartCount(); // Atualiza o contador imediatamente após adicionar o item
     });
 
     cartIcon.addEventListener('click', function () {
         window.location.href = '../sacola.html'; // Ajuste o caminho conforme necessário
     });
 
-    updateCartCount();
+    updateCartCount(); // Atualiza o contador ao carregar a página
 });
